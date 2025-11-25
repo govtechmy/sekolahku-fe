1dbf84151bf99b8b6767e433b591f0b5a78f2311
AMMAR HAIRI <145964801+ammarhairiHD@users.noreply.github.com>
2025-11-25 20:51:02 +0800

    update mobile design

--- src/components/shared/Cards.tsx
+++ src/components/shared/Cards.tsx
@@ -62,7 +62,7 @@ function Card({ children, totalPages: externalTotalPages, currentPage: externalC
   return (
     <HorizontalCardContext.Provider value={contextValue}>
       <div className=" flex flex-col justify-center  gap-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-6">{children}</div>
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-6">{children}</div>
         <div className="flex justify-center">
           <HorizontalCardDots />
         </div>
