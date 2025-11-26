28ef6b1caecce4519556e9f0c5089a7bdabbd77a
muhd afnan <afnan@mydigital.gov.my>
2025-11-26 09:36:41 +0800

    push google map design

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@govtechmy/myds-react/hooks";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <ThemeProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </ThemeProvider>
   </StrictMode>
 );
