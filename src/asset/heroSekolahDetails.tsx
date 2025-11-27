9e0dc747ff4dc8f96bfba6a1aec69854e2856331
Fid GNU <fidaullah@mydigital.gov.my>
2025-11-27 12:04:23 +0800

    update svg

--- src/asset/heroSekolahDetails.tsx
+++ src/asset/heroSekolahDetails.tsx
@@ -13,14 +13,14 @@ export default function HeroSekolahDetails({ className }: HeroLightProps) {
       xmlnsXlink="http://www.w3.org/1999/xlink"
       className={className}
     >
-      <g clip-path="url(#clip0_4_71)">
+      <g clipPath="url(#clip0_4_71)">
         <mask id="path-1-inside-1_4_71" fill="white">
           <path d="M0 0H1440V408H0V0Z" />
         </mask>
 
         <path d="M0 0H1440V408H0V0Z" fill="url(#paint0_radial_4_71)" />
 
-        <g clip-path="url(#clip1_4_71)">
+        <g clipPath="url(#clip1_4_71)">
           <path
             d="M1690.98 -1063.75C2112.22 -1063.75 2453.71 -722.269 2453.71 -301.023C2453.71 120.222 2112.22 461.709 1690.98 461.709C1269.73 461.709 928.245 120.222 928.245 -301.023C928.245 -722.268 1269.73 -1063.75 1690.98 -1063.75Z"
             fill="#F2F8FF"
@@ -28,7 +28,7 @@ export default function HeroSekolahDetails({ className }: HeroLightProps) {
           <path
             d="M1690.98 -1063.75C2112.22 -1063.75 2453.71 -722.269 2453.71 -301.023C2453.71 120.222 2112.22 461.709 1690.98 461.709C1269.73 461.709 928.245 120.222 928.245 -301.023C928.245 -722.268 1269.73 -1063.75 1690.98 -1063.75Z"
             stroke="#FBFDFF"
-            stroke-width="61.5107"
+            strokeWidth="61.5107"
           />
           <path
             d="M1075.87 -301.024C1075.87 -640.738 1351.26 -916.13 1690.98 -916.13C2030.69 -916.13 2306.08 -640.738 2306.08 -301.024C2306.08 38.6903 2030.69 314.083 1690.98 314.083C1351.26 314.083 1075.87 38.6903 1075.87 -301.024Z"
@@ -46,9 +46,9 @@ export default function HeroSekolahDetails({ className }: HeroLightProps) {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(720 0) rotate(90) scale(377.579 1463.69)"
         >
-          <stop stop-color="#E0EDFF" />
-          <stop offset="0.55" stop-color="#FAFCFF" />
-          <stop offset="1" stop-color="white" />
+          <stop stopColor="#E0EDFF" />
+          <stop offset="0.55" stopColor="#FAFCFF" />
+          <stop offset="1" stopColor="white" />
         </radialGradient>
 
         <clipPath id="clip0_4_71">
