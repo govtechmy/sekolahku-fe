6a18c804b8e785e8a5d46b284eb3cb2d94fa4a56
AMMAR HAIRI <145964801+ammarhairiHD@users.noreply.github.com>
2025-12-31 11:19:04 +0800

    SSD-636-redesign analitik components to follow figma

--- src/components/shared/SectionItemAnalytics.tsx
+++ src/components/shared/SectionItemAnalytics.tsx
@@ -1,5 +1,5 @@
 import type { AnalyticsModel } from "../../models/response";
-import { GovtOfficeIcon, BookIcon, UserGroupIcon } from "@govtechmy/myds-react/icon";
+import { PutrajayaIcon, BookIcon, UserGroupIcon } from "@govtechmy/myds-react/icon";
 import DoughnutChart from "../DoughnutChart";
 interface SectionItemAnalyticsProps {
   analytics: AnalyticsModel;
@@ -13,11 +13,11 @@ export default function SectionItemAnalytics(
       <div className="border border-otl-gray-200 rounded-lg overflow-hidden">
         <div className="grid grid-cols-1 lg:grid-cols-3">
           <div className="flex items-center gap-6 px-6 py-8 border-otl-gray-200 border-b lg:border-r lg:border-b-0">
-            <div className="size-16 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
-              <GovtOfficeIcon />
+            <div className="p-4 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
+              <PutrajayaIcon className="w-8 h-8"/>
             </div>
             <div>
-              <div className="text-txt-primary font-body text-body-xl font-semibold">
+              <div className="text-txt-primary font-body text-3xl font-semibold">
                 {analytics?.jumlahSekolah.toLocaleString()}
               </div>
               <div className="text-txt-black-700 font-body text-body-md font-semibold">
@@ -27,11 +27,11 @@ export default function SectionItemAnalytics(
           </div>
 
           <div className="flex items-center gap-6 px-6 py-8 border-otl-gray-200 border-b lg:border-r lg:border-b-0">
-            <div className="size-16 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
-              <BookIcon />
+            <div className="p-4 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
+              <BookIcon className="w-8 h-8"/>
             </div>
             <div>
-              <div className="text-txt-primary font-body text-body-xl font-semibold">
+              <div className="text-txt-primary font-body text-3xl font-semibold">
                 {analytics?.jumlahGuru.toLocaleString()}
               </div>
               <div className="text-txt-black-700 font-body text-body-md font-semibold">
@@ -41,11 +41,11 @@ export default function SectionItemAnalytics(
           </div>
 
           <div className="flex items-center gap-6 px-6 py-8 border-otl-gray-200 border-b last:border-b-0 lg:border-b-0 lg:border-r-0">
-            <div className="size-16 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
-              <UserGroupIcon />
+            <div className="p-4 rounded-full bg-bg-primary-50 text-primary-600 flex items-center justify-center flex-shrink-0">
+              <UserGroupIcon className="w-8 h-8"/>
             </div>
             <div>
-              <div className="text-txt-primary font-body text-body-xl font-semibold">
+              <div className="text-txt-primary font-body text-3xl font-semibold">
                 {analytics?.jumlahPelajar.toLocaleString()}
               </div>
               <div className="text-txt-black-700 font-body text-body-md font-semibold">
