1506d17af5d544ecfc721adf81f8f6a66fd58365
mrlhnf <ameerulhanif@gmail.com>
2026-01-05 11:33:08 +0800

    fix previous commit lint

--- src/services/s3.svc.ts
+++ src/services/s3.svc.ts
@@ -1,30 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
-const PATH = `/file/download`
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const PATH = `/file/download`;
 
 export const getPresignedUrl = async (url: string) => {
   try {
-    const response = await axios.post(`${BASE_URL}${PATH}`, { url })
-    return response.data.data.url as string
+    const response = await axios.post(`${BASE_URL}${PATH}`, { url });
+    return response.data.data.url as string;
   } catch (error) {
-    console.error('Error fetching presigned URL:', error)
-    return url
+    console.error("Error fetching presigned URL:", error);
+    return url;
   }
-}
+};
 
 export const generateDownloadLink = async (fileName: string, url: string) => {
   if (!url) {
-    console.error('No URL provided for download.')
-    return
+    console.error("No URL provided for download.");
+    return;
   }
 
-  const presignedUrl = await getPresignedUrl(url)
-  const link = document.createElement('a')
-  link.href = presignedUrl
-  link.download = fileName
-  link.target = '_blank'
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-}
+  const presignedUrl = await getPresignedUrl(url);
+  const link = document.createElement("a");
+  link.href = presignedUrl;
+  link.download = fileName;
+  link.target = "_blank";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
