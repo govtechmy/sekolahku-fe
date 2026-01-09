914d56221de05d70d98a18603fde3d8a91175db5
harrisazmi <azmi.harris@gmail.com>
2026-01-09 12:52:19 +0800

    fix: handle null/undefined issue

--- src/utils/caseConverter.ts
+++ src/utils/caseConverter.ts
@@ -1,4 +1,4 @@
-export default function caseConverter(value: string): string {
+export default function caseConverter(value?: string | null): string {
   if (!value) return "";
 
   return value.toLowerCase().replace(/\b[a-z]/g, (char) => char.toUpperCase());
