7f6e732331e6dd81a694bfc6afcb36a985e21988
Zaid Aiman <zaidaiman@gmail.com>
2026-01-22 19:07:04 +0800

    various fix based on feedback

--- src/components/shared/SectionHeader.tsx
+++ src/components/shared/SectionHeader.tsx
@@ -14,12 +14,14 @@ export default function SectionHeader({
   header,
   title,
   children,
-  ButtonLabel,  
+  ButtonLabel,
   ButtonClickHandler,
-  isLastSection
+  isLastSection,
 }: SectionHeaderProps) {
   return (
-    <div className={`flex flex-col ${isLastSection ? "" : "pb-16"} px-4 lg:px-[50px]`}>
+    <div
+      className={`flex flex-col ${isLastSection ? "" : "pb-16"} px-4 lg:px-[50px]`}
+    >
       <div className="flex flex-col gap-4">
         <div className="text-txt-primary font-body font-semibold text-sm tracking-[2.8px]">
           {header}
