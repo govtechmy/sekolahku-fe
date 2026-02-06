4199896b4f1f16e53ebabc70154ea1aed07743a0
harrisazmi <azmi.harris@gmail.com>
2026-02-06 23:46:30 +0800

    fix: preventing overlapping req

--- src/store/mapView.ts
+++ src/store/mapView.ts
@@ -153,9 +153,7 @@ export const useMapViewStore = create<MapViewState>((set, get) => ({
       console.error("Error fetching school suggestions:", error);
       set((state) => ({
         localSuggestions: append ? state.localSuggestions : [],
-        hasMoreLocalSuggestions: append
-          ? state.hasMoreLocalSuggestions
-          : false,
+        hasMoreLocalSuggestions: append ? state.hasMoreLocalSuggestions : false,
       }));
     } finally {
       set({ isLoadingLocalSuggestions: false });
