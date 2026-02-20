aaf8ff38895417985ab347b328312ff8049ccfa3
AMMAR HAIRI <145964801+ammarhairiHD@users.noreply.github.com>
2026-02-20 14:43:07 +0800

    eslint for AlertMaps

--- src/store/disclaimerPopup.ts
+++ src/store/disclaimerPopup.ts
@@ -1,22 +1,20 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware"
-
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export interface PopupState {
-    initialPopupState: boolean
-    setInitialPopupState: (status: boolean) => void;
+  initialPopupState: boolean;
+  setInitialPopupState: (status: boolean) => void;
 }
 
-export const usePopupState = create<PopupState>() (
-    persist(
-        (set) => ({
-            initialPopupState: false,
-            setInitialPopupState: (status) => 
-                set({initialPopupState: status})
-        }),
-        {
-            name: "disclaimerPopupSession",
-            storage: createJSONStorage(() => sessionStorage)
-        }
-    )
-)
+export const usePopupState = create<PopupState>()(
+  persist(
+    (set) => ({
+      initialPopupState: false,
+      setInitialPopupState: (status) => set({ initialPopupState: status }),
+    }),
+    {
+      name: "disclaimerPopupSession",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
