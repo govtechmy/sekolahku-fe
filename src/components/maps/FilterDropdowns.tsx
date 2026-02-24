a76a80c91098a6ab8b5f16547b0b3aa53cb45aa6
muhd afnan <afnan@mydigital.gov.my>
2026-02-24 09:57:40 +0800

    add back truncate

--- src/components/maps/FilterDropdowns.tsx
+++ src/components/maps/FilterDropdowns.tsx
@@ -81,7 +81,10 @@ export function FilterDropdowns({
         onValueChange={setSelectedJenis}
         value={selectedJenis ?? "ALL"}
       >
-        <SelectTrigger aria-label="Pilih Jenis" className="w-[155px] justify-between truncate">
+        <SelectTrigger
+          aria-label="Pilih Jenis"
+          className="w-[155px] justify-between truncate"
+        >
           <SelectValue placeholder="Jenis Sekolah" />
         </SelectTrigger>
         <SelectContent className="z-[700]">
