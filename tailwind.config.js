82954d21d4c2dea2f6e7f564c5bfb781dd86a7fb
harrisazmi <azmi.harris@gmail.com>
2026-03-11 10:57:14 +0800

    fix: lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        sans: ["Inter", "system-ui", "-apple-system", "sans-serif"],
       },
     },
   },
