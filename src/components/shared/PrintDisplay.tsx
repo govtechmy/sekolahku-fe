ced16b82e37f298fd539b7ef6c78675bf09a03a2
muhd afnan <afnan@mydigital.gov.my>
2026-03-12 15:58:15 +0800

    update margin conflict

--- src/components/shared/PrintDisplay.tsx
+++ src/components/shared/PrintDisplay.tsx
@@ -13,6 +13,10 @@ export default function PrintDisplay({
             margin: 0cm 2cm 2.5cm 2cm;
             size: A4;
           }
+
+          body {
+            margin: 0 !important;
+          }
           
           .print-content {
             max-width: 100%;
