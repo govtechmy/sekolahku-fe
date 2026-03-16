5270a34959a81e78dd00053affd44eaad24006c2
muhd afnan <afnan@mydigital.gov.my>
2026-03-16 09:33:17 +0800

    fix import on section header

--- src/components/shared/SectionHeader.tsx
+++ src/components/shared/SectionHeader.tsx
@@ -1,5 +1,9 @@
 import { Button, ButtonIcon } from "@govtechmy/myds-react/button";
-import { ArrowForwardIcon, JataNegaraIcon, ArrowOutgoingIcon } from "@govtechmy/myds-react/icon";
+import {
+  ArrowForwardIcon,
+  JataNegaraIcon,
+  ArrowOutgoingIcon,
+} from "@govtechmy/myds-react/icon";
 import { clx } from "@govtechmy/myds-react/utils";
 import { formatEventDateDDMMYY } from "../../utils/date";
 
