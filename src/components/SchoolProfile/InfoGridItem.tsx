28566fc1a2391d911ba82849f97d74457e2f5bb1
Adam Hafidz <hafidzmustafa19@gmail.com>
2026-04-10 16:07:02 +0800

    bugfix - add sesi detail to bil sesi

--- src/components/SchoolProfile/InfoGridItem.tsx
+++ src/components/SchoolProfile/InfoGridItem.tsx
@@ -3,19 +3,22 @@ import { CheckCircleFillIcon, CrossFillIcon } from "@govtechmy/myds-react/icon";
 interface InfoGridItemProps {
   label: string;
   value: string | boolean;
+  note?: string;
 }
 
-export const InfoGridItem = ({ label, value }: InfoGridItemProps) => {
+export const InfoGridItem = ({ label, value, note }: InfoGridItemProps) => {
   return (
     <div
       className="gap-0.5 focus:outline-primary-200"
       tabIndex={0}
-      aria-label={`${label}: ${value}`}
+      aria-label={`${label}: ${value}${note ? ` | ${note}` : ""}`}
     >
       <div className="py-8 px-2.5 rounded-xl shadow text-center">
         <div className="text-txt-primary uppercase pb-2">{label}</div>
         {typeof value === "string" ? (
-          <div className="text-txt-black-500">{value}</div>
+          <div className="text-txt-black-500">
+            {value} {note && `| ${note}`}
+          </div>
         ) : value === true ? (
           <div className="text-txt-success flex items-center justify-center">
             <CheckCircleFillIcon />
