10be22bb482e3e49b8fa09e0a393faf1220b40ff
Zaid Aiman <zaidaiman@gmail.com>
2026-05-17 20:41:22 +0800

    minor adjustment on hero banner and doughnut chart

--- src/components/Hero/HomeHero.tsx
+++ src/components/Hero/HomeHero.tsx
@@ -77,8 +77,14 @@ export default function HomeHero() {
       HeroclassName=""
       background={
         <>
-          <div className="block lg:hidden h-full w-full bg-[url('/utama/kpm-hero-mobile.jpg')] bg-contain bg-center bg-no-repeat" />
-          <div className="hidden lg:block h-full w-full bg-[url('/utama/kpm-hero.jpg')] bg-contain bg-center bg-no-repeat" />
+          <div className="relative block h-full w-full overflow-hidden bg-[#d9ecf7] lg:hidden">
+            <div className="absolute inset-0 scale-110 bg-[url('/utama/kpm-hero-mobile.jpg')] bg-cover bg-center bg-no-repeat opacity-100 blur-2xl" />
+            <div className="absolute inset-0 bg-[url('/utama/kpm-hero-mobile.jpg')] bg-contain bg-center bg-no-repeat" />
+          </div>
+          <div className="relative hidden h-full w-full overflow-hidden bg-[#d9ecf7] lg:block">
+            <div className="absolute inset-0 scale-110 bg-[url('/utama/kpm-hero.jpg')] bg-cover bg-center bg-no-repeat opacity-100 blur-2xl" />
+            <div className="absolute inset-0 bg-[url('/utama/kpm-hero.jpg')] bg-contain bg-center bg-no-repeat" />
+          </div>
         </>
       }
     />
