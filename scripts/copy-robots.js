cb3714b8dd2b8dfed0fe23a901521c9dd9baac79
Zaid Aiman <zaidaiman@gmail.com>
2026-06-24 20:16:22 +0800

    minor fix on build

--- scripts/copy-robots.js
+++ scripts/copy-robots.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const env = process.env.NODE_ENV || "staging";
+const env = process.env.NODE_ENV || "development";
 const sourceFile = path.join(__dirname, `../public/robots.${env}.txt`);
 const destFile = path.join(__dirname, "../public/robots.txt");
 
